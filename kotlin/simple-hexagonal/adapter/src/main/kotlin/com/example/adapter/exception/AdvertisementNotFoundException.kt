40bbec31e8d6a4d03f0cf83395ca6438d4337325
dale <ehrudghl123@naver.com>
2022-04-15 20:41:20 +0900

    안쓰는 클래스 제거

--- kotlin/simple-hexagonal/adapter/src/main/kotlin/com/example/adapter/exception/AdvertisementNotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package com.example.adapter.exception
-
-import com.example.core.advertisement.AdvertisementId
-
-class AdvertisementNotFoundException(override val message: String) : RuntimeException() {
-    constructor(advertiserId: AdvertisementId) : this("광고(${advertiserId.id})를 찾지 못했어요")
-}
