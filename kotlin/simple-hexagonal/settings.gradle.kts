4907c42769b984ebde8650d45a7e20c642bd1a30
dale <ehrudghl123@naver.com>
2022-04-21 19:38:40 +0900

    refactor name

--- kotlin/simple-hexagonal/settings.gradle.kts
+++ kotlin/simple-hexagonal/settings.gradle.kts
@@ -5,7 +5,7 @@ include("domain")
 include("application")
 
 include("adapters:in:webapp")
-include("adapters:in:consumer")
+include("adapters:in:eventapp")
 
 include("adapters:out:external")
 include("adapters:out:persistence")
