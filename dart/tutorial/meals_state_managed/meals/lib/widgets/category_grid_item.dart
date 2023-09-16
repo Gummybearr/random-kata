934e02064ed827368307eac3106e8f373c77534f
Gummybearr <ehrudghl123@naver.com>
2023-09-16 13:44:30 +0900

    feat: navigation

---
+++ dart/tutorial/meals_state_managed/meals/lib/widgets/category_grid_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem(
+      {super.key, required this.category, required this.onSelectCategory});
+
+  final Category category;
+  final void Function() onSelectCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onSelectCategory,
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      ),
+    );
+  }
+}
