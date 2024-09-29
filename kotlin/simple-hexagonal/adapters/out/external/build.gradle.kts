7cfa2fd60175d3a80c4f2ad5b71943b46a844173
Gummybearr <gyunghoe@gyunghoeui-MacBookPro.local>
2024-09-29 11:31:58 +0900

    feat: add google calendar

--- kotlin/simple-hexagonal/adapters/out/external/build.gradle.kts
+++ kotlin/simple-hexagonal/adapters/out/external/build.gradle.kts
@@ -4,4 +4,5 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("javax.transaction:javax.transaction-api:1.3")
+    implementation("com.google.api-client:google-api-client:1.32.1")
 }
