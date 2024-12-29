682cdf43e65ade92ddd1b2482d6c121be90fef09
Gummybearr <ehrudghl123@naver.com>
2024-12-29 15:29:08 +0900

    feat: add tavily

---
+++ python/llm/agents/linkedin_lookup_agent.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import Tool
+from langchain_openai import ChatOpenAI
+
+from tools.tools import get_profile_url_tavily
+
+def lookup(name: str) -> str:
+    llm = ChatOpenAI(
+        temperature=0,
+        model_name="gpt-4o-mini",
+    )
+    template = """
+        given the full name {name_of_person} I want you to get it me a link to their Linkedin profile page.
+        Your answer should contain only a URL
+    """
+
+    prompt_template = PromptTemplate(
+        template=template, input_variables=["name_of_person"]
+    )
+
+    tools_for_agent = [
+        Tool(
+            name="Crawl Google 4 linkedin profile page",
+            func=get_profile_url_tavily,
+            description="useful for when you need to get the Linkedin Page URL",
+        )
+    ]
+
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+
+    result = agent_executor.invoke(
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    linkedin_profile_url = result["output"]
+    return linkedin_profile_url
+
+if __name__ == "__main__":
+    load_dotenv()
+    linkedin_url = lookup(name="Eden Marco")
+    print(linkedin_url)
---
+++ python/llm/tools/tools.py
@@ -0,0 +1,7 @@
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+def get_profile_url_tavily(name: str):
+    """Searches for Linkedin or Twitter Profile Page."""
+    search = TavilySearchResults()
+    res = search.run(f"{name}")
+    return res
